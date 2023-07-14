8e317e88b411ad8a963b345281a38d8da6b46397
VictorYanez <victoryanez11@gmail.com>
2023-07-13 18:10:57 -0600

    Parte de  class EmailService

---
+++ GestionImpresoras/Services/EmailService.cs
@@ -0,0 +1,25 @@
+﻿namespace GestionImpresoras.Services
+{
+    public class EmailService
+    {
+        public void SendEmail(string toName, string toEmail, string subject, string body)
+        {
+            //var message = new MimeMessage();
+            //message.From.Add(new MailboxAddress("Test envio email", "joseandresmont@gmail.com.co"));
+            //message.To.Add(new MailboxAddress(toName, toEmail));
+            //message.Subject = subject;
+            //message.Body = new TextPart("plain")
+            {
+                //Text = body
+            };
+
+            //using (var client = new SmtpClient())
+            {
+                //client.Connect("smtp.gmail.com", 465);
+                //client.Authenticate("joseandresmonto@gmail.com", "elite8359*");
+               // client.Send(message);
+                //client.Disconnect(true);
+            }
+        }
+    }
+}
